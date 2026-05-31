bd8d5b3c2e4e2e8f397f92237fe3bb5747584df3
samw060 <sam.wilkinson935@gmail.com>
2026-05-31 11:55:12 +0100

    added javadocs for WeatherExceptions

--- src/main/java/com/sam/weather/weatherapp/WeatherException.java
+++ src/main/java/com/sam/weather/weatherapp/WeatherException.java
@@ -1,19 +1,30 @@
 package com.sam.weather.weatherapp;
 
 /**
- * This class is how I handle the different error codes I can get from the HTTP request.
- * It extends runtime-exception as this is an unchecked exception it will only be seen at run time
- * so we don't need to wrap it in try catch, or write throws throughout the code.
+ * Exception thrown when a weather API request fails.
+ * Contains HTTP status code and error message.
  */
 public class WeatherException extends RuntimeException {
 
+    /** The HTTP status code of the associated error. */
     private int statusCode;
 
+    /**
+     * Constructs a new WeatherException with the error message and status code.
+     *
+     * @param message the description of the error.
+     * @param statusCode the code related to the error.
+     */
     public WeatherException(String message, int statusCode){
         super(message);                     // As a runtime-exception can take a message in constructor, super() passes it into message field.
         this.statusCode = statusCode;
     }
 
+    /**
+     * Returns the status code of the error.
+     *
+     * @return the status code.
+     */
     public int getStatusCode(){
         return statusCode;
     }
